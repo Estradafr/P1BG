e7bece95f000fe5de04e3040ed3d6132d6f12110
Fernando Estrada <Estradafr@yahoo.com>
2023-01-20 09:25:47 -0800

    update: refactored some JS. Updated GUI for game

---
+++ main.js
@@ -0,0 +1,19 @@
+// Variables
+let level = 1;
+let charClass = "";
+let health = 100;
+let gold = 50;
+let inventory = [];
+let wield = [];
+
+// * Query Selectors 
+const leftButton = document.querySelector('#leftButton');
+const middleButton = document.querySelector('#middleButton');
+const rightButton = document.querySelector('#rightButton');
+const topText = document.querySelector('#topText');
+const bottomText = document.querySelector('#bottomText');
+const levelNum = document.querySelector('#levelNum');
+const healthNum = document.querySelector('#healthNum');
+const goldNum = document.querySelector('#goldNum');
+
+// * Path Locations
